45fc3bd80a9e6b1327e3f88006008fc9a9929f05
Matt <mhk4g@virginia.edu>
2016-03-30 18:25:55 -0400

    More improvements to inserts, not yet done

--- create_inserts.php
+++ create_inserts.php
@@ -1,8 +1,9 @@
 <?php 
 include_once("./got_wiki_parser.php"); 
+include_once("./process_inserts.php");
 
-//$characters = ['Eddard_Stark', 'Catelyn_Stark', 'Robb_Stark'];
-$characters = ['Eddard_Stark', 'Catelyn_Stark', 'Robb_Stark', 'Sansa_Stark', 'Arya_Stark', 'Bran_Stark', 'Rickon_Stark', 'Jon_Snow', 'Brynden_Tully', 'Edmure_Tully', 'Tywin_Lannister', 'Cersei_Lannister', 'Jaime_Lannister', 'Tyrion_Lannister', 'Joffrey_Baratheon', 'Myrcella_Baratheon', 'Tommen_Baratheon', 'Kevan_Lannister', 'Lancel_Lannister', 'Bronn', 'Qyburn', 'Pycelle', 'Petyr_Baelish', 'Varys', 'House_Tyrell', 'Margaery_Tyrell', 'Loras_Tyrell', 'Olenna_Tyrell', 'Mace_Tyrell', 'House_Bolton', 'Roose_Bolton', 'Ramsay_Bolton', 'Theon_Greyjoy', 'Walda_Bolton', 'House_Frey', 'Walder_Frey', 'Lothar_Frey', 'Walder_Rivers', 'Roslin_Frey', 'Robert_Baratheon', 'Stannis_Baratheon', 'Renly_Baratheon', 'Brienne_of_Tarth', 'Selyse_Baratheon', 'Shireen_Baratheon', 'Gendry', 'Davos_Seaworth', 'Salladhor_Saan', 'Melisandre', 'Daenerys_Targaryen', 'Viserys_Targaryen', 'Aerys_II_Targaryen', 'Rhaegar_Targaryen', 'Aemon', 'Drogo', 'Barristan_Selmy', 'Missandei', 'Grey_Worm', 'Daario_Naharis', 'Doran_Martell', 'Trystane_Martell', 'Elia_Martell', 'Oberyn_Martell', 'Ellaria_Sand', 'Obara_Sand', 'Nymeria_Sand', 'Tyene_Sand', 'Areo_Hotah', 'Gregor_Clegane', 'Sandor_Clegane', 'Beric_Dondarrion', 'Jaqen_H\'ghar','Thoros', 'Syrio_Forel', 'Craster', 'Quaithe', 'Xaro', 'Jorah_Mormont', 'Jeor_Mormont', 'Shae', 'Lysa_Arryn', 'Robin_Arryn', 'Spice_King', 'Pyat_Pree', 'Podrick_Payne', 'Ilyn', 'Janos_Slynt', 'Qhorin', 'Mance_Rayder', 'Alliser_Thorne', 'Samwell_Tarly', 'Gilly', 'Locke', 'Ygritte', 'Tormund', 'Meera_Reed', 'Jojen_Reed', 'Balon_Swann', 'Balon_Greyjoy', 'Yara_Greyjoy', 'Osha', 'Luwin', 'Hodor', 'Lord_of_Bones', 'Jon_Arryn', 'Illyrio_Mopatis', 'Gared', 'Hot_Pie', 'Lommy_Greenhands', 'Polliver', 'Lyanna_Stark', 'Mycah', 'High_Sparrow', 'Ros', 'Mhaegen', 'Armeca', 'Shagga', 'Timett', 'Chella', 'Talisa_Stark', 'Myranda', 'Violet', 'Tansy'];
+$characters = ['Eddard_Stark', 'Catelyn_Stark', 'Robb_Stark'];
+//$characters = ['Eddard_Stark', 'Catelyn_Stark', 'Robb_Stark', 'Sansa_Stark', 'Arya_Stark', 'Bran_Stark', 'Rickon_Stark', 'Jon_Snow', 'Brynden_Tully', 'Edmure_Tully', 'Tywin_Lannister', 'Cersei_Lannister', 'Jaime_Lannister', 'Tyrion_Lannister', 'Joffrey_Baratheon', 'Myrcella_Baratheon', 'Tommen_Baratheon', 'Kevan_Lannister', 'Lancel_Lannister', 'Bronn', 'Qyburn', 'Pycelle', 'Petyr_Baelish', 'Varys', 'House_Tyrell', 'Margaery_Tyrell', 'Loras_Tyrell', 'Olenna_Tyrell', 'Mace_Tyrell', 'House_Bolton', 'Roose_Bolton', 'Ramsay_Bolton', 'Theon_Greyjoy', 'Walda_Bolton', 'House_Frey', 'Walder_Frey', 'Lothar_Frey', 'Walder_Rivers', 'Roslin_Frey', 'Robert_Baratheon', 'Stannis_Baratheon', 'Renly_Baratheon', 'Brienne_of_Tarth', 'Selyse_Baratheon', 'Shireen_Baratheon', 'Gendry', 'Davos_Seaworth', 'Salladhor_Saan', 'Melisandre', 'Daenerys_Targaryen', 'Viserys_Targaryen', 'Aerys_II_Targaryen', 'Rhaegar_Targaryen', 'Aemon', 'Drogo', 'Barristan_Selmy', 'Missandei', 'Grey_Worm', 'Daario_Naharis', 'Doran_Martell', 'Trystane_Martell', 'Elia_Martell', 'Oberyn_Martell', 'Ellaria_Sand', 'Obara_Sand', 'Nymeria_Sand', 'Tyene_Sand', 'Areo_Hotah', 'Gregor_Clegane', 'Sandor_Clegane', 'Beric_Dondarrion', 'Jaqen_H\'ghar','Thoros', 'Syrio_Forel', 'Craster', 'Quaithe', 'Xaro', 'Jorah_Mormont', 'Jeor_Mormont', 'Shae', 'Lysa_Arryn', 'Robin_Arryn', 'Spice_King', 'Pyat_Pree', 'Podrick_Payne', 'Ilyn', 'Janos_Slynt', 'Qhorin', 'Mance_Rayder', 'Alliser_Thorne', 'Samwell_Tarly', 'Gilly', 'Locke', 'Ygritte', 'Tormund', 'Meera_Reed', 'Jojen_Reed', 'Balon_Swann', 'Balon_Greyjoy', 'Yara_Greyjoy', 'Osha', 'Luwin', 'Hodor', 'Lord_of_Bones', 'Jon_Arryn', 'Illyrio_Mopatis', 'Gared', 'Hot_Pie', 'Lommy_Greenhands', 'Polliver', 'Lyanna_Stark', 'Mycah', 'High_Sparrow', 'Ros', 'Mhaegen', 'Armeca', 'Shagga', 'Timett', 'Chella', 'Talisa_Stark', 'Myranda', 'Violet', 'Tansy'];
 $base_url = "http://gameofthrones.wikia.com/wiki/";
 $dictionaries = [];
 
@@ -13,7 +14,8 @@ echo "Total entries: " . count($dictionaries) . "\n";
 print_r($dictionaries);
 
 
-convert_all_characters($dictionaries);
+$cmds = convert_all_characters($dictionaries);
+process_inserts($cmds);
 
 // Take the wiki parser's output dictionary as a parameter
 function convert_all_characters($parser_output_array) {
@@ -28,14 +30,35 @@ function convert_all_characters($parser_output_array) {
     return $SQLcommands;
 }
 
-function convert_single_character_to_SQL($character_array) {
+function convert_single_character_to_SQL($ch) {
     
     // The outermost array is a key:value pair with char name mapped to array containing every other k:v pair
-    //$name = array_keys($character_array)[0];
-    //echo $name . "\n";
+    // $name = array_keys($character_array)[0];
+    // echo $name . "\n";
     
-    $CHARACTER_SCHEMA = ['name', 'actor_name', 'faction', 'first_appearance'];
-    $WIKI_KEYS = ['Portrayed by', 'First seen']; 
+    # Character table
+    $name = $ch['Name'];
+    $appears = $ch['First seen'];
+    $alive = $ch['Status'];
+    $character_insert = "INSERT INTO Characters (name, actor_name, first_appearance, status) VALUES ('$name', '$actor', '$appears', '$alive')";
+    
+    # CharacterActor table
+    # For each ...
+        # bind
+        # $character_actor_insert =  "INSERT INTO CharacterActor (character_name, actor_name) VALUES ()";
+    
+    # CharacterAlias table
+    # For each ...
+        # bind
+         # $character_alias_insert =  "INSERT INTO CharacterAlias (character_name, alias_name) VALUES ()";
+    
+    # CharacterFaction table
+    # For each ...
+        # bind
+        # $character_faction_insert =  "INSERT INTO CharacterFaction (character_name, faction_name) VALUES ()";
+    
+    # Will need to adjust to merge each array into the master array
+    return $character_insert;
     
     # Start with base SQL statement
     $CHARACTER_BASE = "INSERT INTO Characters(name, actor_name, faction, first_appearance) VALUES (";
