060fc2e382e656671b886a60a14d562447647dc2
mhk4g <mhk4g@labunix01.cs.virginia.edu>
2016-04-06 22:15:24 -0400

    AH HA

--- process_search.php
+++ process_search.php
@@ -1,6 +1,7 @@
 <?php 
+ini_set('display_errors', 1);
+echo("<html>Hi");
 
-//header("access-control-allow-origin: *");
 $userinput = "targaryen";
 
 # TEST DATA
@@ -22,7 +23,6 @@ $dbpass = "aryastark";
 $dbname = "cs4750mhk4g";
 $HTTPresponse = array();    # <- This is where AJAX response data goes, as K/V pairs
 
-#TODO:
 # Use user type as a check for certain types of query
 
 // Construct SQL query using input
@@ -34,17 +34,20 @@ $HTTPresponse = array();    # <- This is where AJAX response data goes, as K/V p
 // Print results
 //print($userinput);
 
-# Database connection
 $db = new mysqli('stardock.cs.virginia.edu', $dbuser, $dbpass, $dbname);
 if ($db->connect_error) {
     die("Could not connect to database: " . $db->connect_error);
   }
 
-# SANITIZE USER INPUT
-
 $HTTPResponse = [];
 $result = $db->query("SELECT * FROM Characters WHERE name LIKE'%$userinput%'");
-$result_array = mysqli_fetch_all($result);
+$result_array = [];
+while ($data = $result->fetch_array())
+{
+    $result_array[] = $data;
+}
+
+//$result_array = $result.fetch_all();
 // echo("<table border = \"1\" cellpadding = \"8\" width=\"50%\" align=\"center\">");
 // echo("<caption><h2>Search results</h2></caption>");
 // echo("<tr align = \"center\">");
@@ -74,60 +77,21 @@ foreach ($HTTPResponse as $h) {
 // endforeach;
 // $HTTPresponse["alldone"] = "YEP";
 
-# Get final score from session
 // $final_score = $num_correct_so_far;
 
 // $HTTPresponse["finalscore"] = (int)$final_score;
 // $HTTPresponse["finalattempted"] = (int)$number_of_questions;
 // $temppercentage = (float)($final_score / $number_of_questions)*100;
-// $score_percentage = number_format((float)$temppercentage, 2, '.', '');  
 // $HTTPresponse["percentage"] = $score_percentage;
 
 // $num_ppl_completed = (int)$the_quiz["numusers"];
 // $num_ppl_completed++;
 // $oldpublicavg = (float)strip_tags($the_quiz["publicavg"]);
 // $newpublicavg = (float)($oldpublicavg*($num_ppl_completed - 1) + $score_percentage) / $num_ppl_completed;
-// $formattednewpublicavg = number_format((float)$newpublicavg, 2, '.', ''); 
 // $HTTPresponse["publicavg"] = $formattednewpublicavg;
-
-# Add string field to Quizzes table
-# Get the string, decode it, for each index, add to a running sum 
 
 // $scores_array = json_decode($the_quiz["scoresarray"]);
 // $scores_array[] = $score_percentage;
 // $running_sum = (float)0.0;
 // for($i = 0; $i < $num_ppl_completed; $i++) {
-// $running_sum += (float)$scores_array[$i];   
-// }
-  
-# Encodes the scores array for the database
-// $encoded_scores = json_encode($scores_array);
-  
-  # Update DB to reflect new stats
-#  $finaladd = $db->query("UPDATE Quizzes SET publicavg = '$formattednewpublicavg', numusers = '$num_ppl_completed', scoresarray = '$encoded_scores' WHERE datestring = '$date'");
-    
-  # Encode and echo
-#  $finalencode = json_encode($HTTPresponse);
-#  echo($finalencode);
-#  }
-  
-# Add the next question
-// $HTTPresponse["category"] = $temparray["category"];
-// $HTTPresponse["currentQuestionID"] = (int)strip_tags($temparray["ID"]);
-// $HTTPresponse["questiontext"] = strip_tags($temparray["string"]);
-// $HTTPresponse["numchoices"] = (int)strip_tags($temparray["numchoices"]);
-
-# Add the answers for the next question
-// $result = $db->query("SELECT * FROM Answers WHERE questionID='$currentquestionID'");
-// $resultrows = mysqli_num_rows($result);
-// for($i = 1; $i < $resultrows+1; $i++) {
-//   $temparray = $result->fetch_assoc();
-//   $HTTPresponse["answer" . $i] = strip_tags($temparray["string"]);
-//   $HTTPresponse["answerID" . $i] = (int)strip_tags($temparray["ID"]);
-// }
-
-# JSON Encode and send all data to dailyquiz.php
-// $finalEncode = json_encode($HTTPresponse);
-// echo($finalEncode);
-// }
 ?>
