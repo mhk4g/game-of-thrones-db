373d084bcb07973595ad42c57c726973f1f91478
Matt <mhk4g@virginia.edu>
2016-04-20 15:25:49 -0400

    Gettin everything workin

--- login_page.php
+++ login_page.php
@@ -1,58 +1,12 @@
 <?php 
 session_start();
 
-if isset($_SESSION["error"]) {
+if (isset($_SESSION["error"])) {
     $error_display = $_SESSION["error"];
 }
-
  ?>
 
-
 <html background-color>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-<script type="text/javascript" language="javascript">
-
-function sendRequest() {
-    var httpRequest;
-    var post_string = prepareUserInput();
-    
-    // BEGIN OBLIGATORY AJAX STUFF //
-    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-        httpRequest = new XMLHttpRequest();
-        if (httpRequest.overrideMimeType) {
-            httpRequest.overrideMimeType('text/xml'); } } 
-    else if (window.ActiveXObject) { // IE
-        try { httpRequest = new ActiveXObject("Msxml2.XMLHTTP"); } 
-            catch (e) { try { httpRequest = new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) {} } }
-    if (!httpRequest) { alert('Giving up :( Cannot create an XMLHTTP instance'); return false; }
-    // END OBLIGATORY AJAX STUFF //
-    
-    httpRequest.onreadystatechange = function() { displayResults(httpRequest); };
-    httpRequest.open('POST', 'process_search.php', true);
-    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    httpRequest.send(post_string);
-  }
-  
-function displayResults(httpRequest) {
-  if (httpRequest.readyState == 4) {
-      if (httpRequest.status == 200) {
-          document.getElementById("searchresults").innerHTML = httpRequest.responseText;
-          document.getElementById("specialinput").value = "";
-        } 
-    }
-  }
-
-function prepareUserInput() {
-    temp_string = "userinput=" + document.getElementById("specialinput").value;
-    return temp_string;
-}
-
-$(document).keypress(function(e) {
-        if(e.which == 13) {
-                sendRequest();
-        }
-        });
-
 </script>
 <head>
     <link type="text/css" rel="stylesheet" href="login_style.css"/>
@@ -60,13 +14,14 @@ $(document).keypress(function(e) {
 </head>
 <body bgcolor="black">
     <div class="input-box" width=200px>
-        <p class="login-label">Email</p><input class="login-field" id="email" name="email" type="text" placeholder="Enter your email address" autofocus="autofocus" autocomplete="off" required width="200px"/>
-        <br>
-        <p class="login-label">Password</p><input class="login-field" id="password" name="password" type="password" placeholder="Enter your password" autocomplete="off" required />
-        <br><br>
-        <input type="submit" id="login" name="login" value="Login">
-        <br><br>
-        <input type="button" value="Create Account">
+        <form action="process_login.php" method="post">
+        <p class="login-label">Email</p><input class="login-field" id="email" name="email" type="text" placeholder="Enter your email address" autofocus="autofocus" autocomplete="off" required width="200px"/><br>
+        <p class="login-label">Password</p><input class="login-field" id="password" name="password" type="password" placeholder="Enter your password" autocomplete="off" required /><br><br>
+        <input type="submit" id="login" name="login" value="Login"><br><br>
+        <input type="button" value="Create Account"><br>
+        <p id="error"><?php echo($error_display) ?></p>
         <br><br><p class="proceed">Proceed without logging in</p>
+        </form>
     </div>
+</form>
 </body>
--- process_login.php
+++ process_login.php
@@ -1,17 +1,18 @@
 <?php 
 session_start();
+session_unset();
 
 
 # If either field omitted, display error
-if(empty($_POST["username"]) || empty($_POST["password"])):
+if(empty($_POST["email"]) || empty($_POST["password"])):
   $_SESSION["error"] = "Please enter an email address and password.";
   header("Location: login_page.php");
   die;
 endif;
 
 # Get login info from POST
-if(isset($_POST["username"])) {
-    $user = (string)$_POST["username"];
+if(isset($_POST["email"])) {
+    $user = (string)$_POST["email"];
 }
 if(isset($_POST["password"])) {
     $password = (string)$_POST["password"];
@@ -28,7 +29,7 @@ $dbpass = "aryastark";
 $dbname = "cs4750mhk4g";
 $hashedpw = hash("md5", $password);
 
-$db = new mysqli('localhost', $dbuser, $dbpass, $dbname);
+$db = new mysqli('stardock.cs.virginia.edu', $dbuser, $dbpass, $dbname);
 if ($db->connect_error) {
     die("Could not connect to database: " . $db->connect_error);
   }
@@ -47,7 +48,7 @@ if(isset($_POST["login"])) {
   
   else:
     
-    $_SESSION["error"] = "Invalid email address or password.";
+    $_SESSION["error"] = "Invalid email address or password: $temp \n $user \n $hashedpw";
     header("Location: login_page.php");
     
   endif;
