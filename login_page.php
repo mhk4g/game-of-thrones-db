8b651b891d274befbf00f1a6b4ed945c1d987c85
Matt <mhk4g@virginia.edu>
2016-04-19 22:11:18 -0400

    Almost finish login page

--- login_page.php
+++ login_page.php
@@ -45,12 +45,18 @@ $(document).keypress(function(e) {
 
 </script>
 <head>
-    <link type="text/css" rel="stylesheet" href="style.css"/>
+    <link type="text/css" rel="stylesheet" href="login_style.css"/>
     <title>Game of Thrones DB: Login</title>
 </head>
 <body bgcolor="black">
-    <div class="input-box" width=100px>
-        <input id="email" name="email" type="text" placeholder="Enter your email address" autofocus="autofocus" autocomplete="off" required /><p><p>
-        <input id="password" name="password" type="password" placeholder="Enter your password" autocomplete="off" required />
+    <div class="input-box" width=200px>
+        <p class="login-label">Email</p><input class="login-field" id="email" name="email" type="text" placeholder="Enter your email address" autofocus="autofocus" autocomplete="off" required width="200px"/>
+        <br>
+        <p class="login-label">Password</p><input class="login-field" id="password" name="password" type="password" placeholder="Enter your password" autocomplete="off" required />
+        <br><br>
+        <input type="submit" id="submit" name="submit" value="Login">
+        <br><br>
+        <input type="button" value="Create Account">
+        <br><br><p class="proceed">Proceed without logging in</p>
     </div>
 </body>
