2cc18c559e78f677aeeb00a3530ab8b48f1d40ee
Matt <mhk4g@virginia.edu>
2016-04-20 23:08:38 -0400

    Fix account registration fields

--- register_page.php
+++ register_page.php
@@ -17,8 +17,8 @@ if (isset($_SESSION["error"])) {
     <div class="input-box" width=200px>
         <form action="process_register.php" method="post">
             <p class="login-label">Email: </p><input class="login-field" id="email" name="username" type="text" placeholder="Enter your email address" autofocus="autofocus" autocomplete="off" required width="200px"/><br>
-            <p class="login-label">Password: </p><input class="login-field" id="password" name="password" type="text" placeholder="Enter your desired password" autofocus="autofocus" autocomplete="off" required width="200px"/><br>
-            <p class="login-label">Confirm Password: </p><input class="login-field" id="password2" name="password2" type="text" placeholder="Enter your password again" autofocus="autofocus" autocomplete="off" required width="200px"/><br><br>
+            <p class="login-label">Password: </p><input class="login-field" id="password" name="password" type="password" placeholder="Enter your desired password" autofocus="autofocus" autocomplete="off" required width="200px"/><br>
+            <p class="login-label">Confirm Password: </p><input class="login-field" id="password2" name="password2" type="password" placeholder="Enter your password again" autofocus="autofocus" autocomplete="off" required width="200px"/><br><br>
             <input type="submit" id="register" name="register" value="Register"><br><br>
         <p id="error"><?php echo($error_display) ?></p>
         <br><br><p class="proceed">Proceed without registering</p>
