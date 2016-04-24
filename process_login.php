47f9aa2933f04b4e11a5837a1ff895e51e25044b
Matt <mhk4g@virginia.edu>
2016-04-24 19:52:50 -0400

    please work pt 2

--- process_login.php
+++ process_login.php
@@ -41,7 +41,7 @@ if(isset($_POST["login"])) {
   
   else:
     
-    $_SESSION["error"] = "Invalid email address or password: $temp \n $user \n $hashedp";
+    $_SESSION["error"] = "Invalid email address or password: $temp \n $user \n $hashedpw";
     header("Location: login_page.php");
     
   endif;
