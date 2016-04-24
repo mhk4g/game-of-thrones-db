44c148d85a21e8fe34b942cdfdd82afc08e975f0
Matt <mhk4g@virginia.edu>
2016-04-24 19:31:44 -0400

    Uhhh

--- about_page.php
+++ about_page.php
@@ -15,7 +15,7 @@ if (isset($_SESSION["email_address"])) {
 }
 
 if (isset($_SESSION["access_level"])) {
-    $login_label = $_SESSION["access_level"];
+    $access_level = $_SESSION["access_level"];
 } else {
     $access_level = 4;
 }
